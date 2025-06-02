3f91bad98f189cbeb0afb144679226c9426314e8
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-02 21:13:19 +0300

    Добавлена обработка HTTP-запросов в MyWebServer
    В файле `MyWebServer.cs` реализованы классы и методы для обработки HTTP-запросов, включая класс `HttpRequest` и методы для формирования ответов на запросы и ошибки 404. Метод `HandleRequest` теперь использует `NetworkStream` для работы с клиентами и поддерживает JSON-ответы.
    
    В файле `Program.cs` добавлен импорт пространства имен `TcpChatServer` и создан экземпляр `MyWebServer` для обработки запросов. Удален устаревший код, что упростило логику обработки запросов.

---
+++ TcpChatServer/MyWebServer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TcpChatServer;
+
+public class MyWebServer
+{
+    class HttpRequest
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string HttpVersion { get; set; }
+        public Dictionary<string, string> Headers { get; set; } = new();
+        public string Body { get; set; } = string.Empty;
+    }
+
+    HttpRequest GetRequest(StreamReader reader)
+    {
+        // Читач даних
+        string? requestLine = reader.ReadLine();
+        Console.WriteLine($"{requestLine}");
+        // Треба отримати метод запиту, шлях та версію HTTP
+        var request = new HttpRequest()
+        {
+            Method = requestLine?.Split(' ')[0] ?? string.Empty, // GET
+            Path = requestLine?.Split(' ')[1] ?? string.Empty, // /about
+            HttpVersion = requestLine?.Split(' ')[2] ?? "HTTP/1.1" // HTTP/1.1
+        };
+        return request;
+    }
+
+    string NotFoundErrorResponse()
+    {
+        string htmlBody = """
+<!DOCTYPE html>
+<html>
+<head><title>My web site</title></head>
+<body>
+    <h1>NOT FOUND</h1>
+    <p>Не знайдено</p>
+<p><a href="/">Main page</a></p>
+</body>
+</html>
+""";
+
+        string httpResponse =
+            "HTTP/1.1 404 NOT FOUND\r\n" +
+            "Date: Mon, 02 Jun 2025 20:11:00 GMT\r\n" +
+            "Server: CustomServer/1.0\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            $"Content-Length: {Encoding.UTF8.GetByteCount(htmlBody)}\r\n" +
+            "Connection: close\r\n" +
+            "\r\n" + // Пустая строка между заголовками и телом
+            htmlBody;
+
+        return httpResponse;
+    }
+
+
+    string MainPageResponse()
+    {
+        string htmlBody = """
+<!DOCTYPE html>
+<html>
+<head><title>My web site</title></head>
+<body>
+    <h1>Main page</h1>
+    <p><a href="/about">About me</a></p>
+</body>
+</html>
+""";
+
+        string httpResponse =
+            "HTTP/1.1 200 OK\r\n" +
+            "Date: Mon, 02 Jun 2025 20:11:00 GMT\r\n" +
+            "Server: CustomServer/1.0\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            $"Content-Length: {Encoding.UTF8.GetByteCount(htmlBody)}\r\n" +
+            "Connection: close\r\n" +
+            "\r\n" + // Пустая строка между заголовками и телом
+            htmlBody;
+
+        return httpResponse;
+    }
+
+    public void HandleRequest(NetworkStream stream)
+    {
+        // Читач даних
+        using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+        using StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+        var request = GetRequest(reader);
+        
+        if (request.Path == "/about")
+        {
+            
+            
+        }
+
+        if (request.Path == "/")
+        {
+            writer.Write(MainPageResponse());
+            writer.Flush();
+            return;
+        }
+
+        writer.Write(NotFoundErrorResponse());
+        writer.Flush();
+        return;
+
+
+        if (request.Path == "/photo.jpeg")
+        {
+            
+        }
+
+
+
+        // Формуємо відповідь сервера в форматі json
+        var data = new { Message = "Hello world" };
+        string jsonBody = JsonSerializer.Serialize(data);
+
+
+        // Формуємо відповідь сервера в вигляді HTML-сторінки
+
+
+
+
+
+        //writer.Write(jsonResponse);
+        
+    }
+
+
+}
--- TcpChatServer/Program.cs
+++ TcpChatServer/Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
+using TcpChatServer;
 
 Console.WriteLine("Server");
 
@@ -21,87 +22,18 @@ while (true)
     TcpClient client = server.AcceptTcpClient();
     Console.WriteLine("Client connected");
 
+    var myWebServer = new MyWebServer();
+
     Thread clientThread = new Thread(() =>
     {
+        
         // Поток обробки клієнта
 
         // Мережевий поток для роботи з клієнтом
         NetworkStream stream = client.GetStream();
+        myWebServer.HandleRequest(stream);
 
-        // Читач даних
-        using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-        //
-        using StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
-
-
-        string? requestLine = reader.ReadLine(); 
-        // GET /about HTTP/1.1
-        Console.WriteLine($"{requestLine}");
-
-        Dictionary<string, string> headers = new();
-
-        string? line;
-        while (!string.IsNullOrEmpty(line = reader.ReadLine()))
-        {
-            Console.WriteLine(line);
-            var parts = line.Split(":", 2);
-            if (parts.Length == 2)
-                headers[parts[0].Trim()] = parts[1].Trim();
-        }
-
-        // (опционально) Читаем тело запроса
-        string? body = null;
-        if (headers.TryGetValue("Content-Length", out string? lengthValue) && int.TryParse(lengthValue, out int contentLength))
-        {
-            char[] buffer = new char[contentLength];
-            int read = reader.Read(buffer, 0, contentLength);
-            body = new string(buffer, 0, read);
-
-            Console.WriteLine("Тело запроса:");
-            Console.WriteLine(body);
-        }
-
-        // Формуємо відповідь сервера в форматі json
-        var data = new {Message = "Hello world"};
-        string jsonBody = JsonSerializer.Serialize(data);
-
-
-        // Формуємо відповідь сервера в вигляді HTML-сторінки
-
-        string htmlBody = """
-<!DOCTYPE html>
-<html>
-<head><title>Example Page</title></head>
-<body>
-    <h1>Hello world</h1>
-    <p>Це наш власний web сервер</p>
-</body>
-</html>
-""";
-
-        string httpResponse =
-            "HTTP/1.1 200 OK\r\n" +
-            "Date: Mon, 02 Jun 2025 20:11:00 GMT\r\n" +
-            "Server: CustomServer/1.0\r\n" +
-            "Content-Type: text/html; charset=UTF-8\r\n" +
-            $"Content-Length: {Encoding.UTF8.GetByteCount(htmlBody)}\r\n" +
-            "Connection: close\r\n" +
-            "\r\n" + // Пустая строка между заголовками и телом
-            htmlBody;
-
-        string jsonResponse =
-           "HTTP/1.1 200 OK\r\n" +
-           "Date: Mon, 02 Jun 2025 20:11:00 GMT\r\n" +
-           "Server: CustomServer/1.0\r\n" +
-           "Content-Type: application/json; charset=UTF-8\r\n" +
-           $"Content-Length: {Encoding.UTF8.GetByteCount(jsonBody)}\r\n" +
-           "Connection: close\r\n" +
-           "\r\n" + // Пустая строка между заголовками и телом
-           jsonBody;
-
-        //writer.Write(httpResponse);
-        writer.Write(jsonResponse);
-        writer.Flush();
+        
 
         //var message = Encoding.UTF8.GetString(MyNetworkHerper.GetDataBlock(stream));
         //Console.WriteLine($"Received message: {message}");
