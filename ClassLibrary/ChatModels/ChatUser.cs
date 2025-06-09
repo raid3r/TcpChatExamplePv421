8de52266f2d92d22ccf371f36ff566a4e181647c
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-09 19:39:02 +0300

    Добавлены модели для чата и аутентификации пользователей
    Внесены изменения в несколько файлов, связанных с моделью чата.
    Добавлены классы для представления пользователей и запросов:
    - `ChatUser` с свойствами `UserId` и `Login`.
    - `GetUsersRequest` для запроса списка пользователей.
    - `GetUsersResponse` с списком пользователей (`Users`) типа `ChatUser`.
    - `LoginRequest` с полями `Login` и `Password` для аутентификации.
    - `LoginResponse` с `UserId` и `AuthToken` для передачи информации после входа.

---
+++ ClassLibrary/ChatModels/ChatUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.ChatModels;
+
+public class ChatUser
+{
+    public int UserId { get; set; } // Унікальний ідентифікатор користувача
+    public string Login { get; set; } = string.Empty; // Ім'я користувача
+}
---
+++ ClassLibrary/ChatModels/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.ChatModels;
+
+public class LoginRequest
+{
+    public string Login { get; set; } = string.Empty; // Логін користувача
+    public string Password { get; set; } = string.Empty; // Пароль користувача
+}
