a350238da637abc0f110e0e536ee8f46669224d5
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-09 19:39:50 +0300

    Добавить функции входа и регистрации в чат-клиент
    Реализованы функции для входа и регистрации пользователя.
    Программа теперь запрашивает выбор между входом и регистрацией,
    а также логин и пароль. В зависимости от выбора выполняется
    соответствующая операция. Добавлен вывод списка пользователей
    после завершения операции.

--- TcpChatClient/Program.cs
+++ TcpChatClient/Program.cs
@@ -7,11 +7,38 @@ using TcpChatClient;
 
 
 var chatClient = new MyChatClient(IPAddress.Parse("127.0.0.1"), 5001);
+//var login = "user1";
+//var password = "password";
 
-var login = "user1";
-var password = "password";
+Console.WriteLine("Chat Client");
+Console.WriteLine("1- Login, 2 - Register");
+var choice = Console.ReadLine();
+if (choice != "1" && choice != "2")
+{
+    Console.WriteLine("Invalid choice. Exiting.");
+    return;
+}
 
-chatClient.Register(login, password);
+Console.Write("Login: ");
+var login = Console.ReadLine();
+
+Console.Write("Password: ");
+var password = Console.ReadLine();
+
+if (choice == "1")
+{
+    chatClient.Login(login, password);
+}
+else
+{
+    chatClient.Register(login, password);
+}
+
+var users = chatClient.GetUsers();
+users.ForEach(user => Console.WriteLine($"Id: {user.UserId} login: {user.Login}"));
+
+
+
 
 Console.ReadLine();
 
