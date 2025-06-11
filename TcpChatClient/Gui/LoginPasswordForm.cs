9e258459a153acdd503b9a25d9fdee5448a5ddd5
Vasyl Kovalov <raid3r81@gmail.com>
2025-06-11 20:53:32 +0300

    Добавлен класс LoginPasswordForm для ввода данных
    В коде реализован новый класс `LoginPasswordForm`, который включает свойства для заголовка формы, логина и пароля. Также добавлен метод `Show`, который очищает консоль, выводит заголовок и запрашивает у пользователя ввод логина и пароля.

---
+++ TcpChatClient/Gui/LoginPasswordForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChatClient.Gui;
+
+public class LoginPasswordForm
+{
+    public string Title { get; set; } = "Login Form";
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public void Show()
+    {
+        Console.Clear();
+        Console.WriteLine(Title);
+        Console.WriteLine(new string('-', 20));
+        
+        Console.Write("Enter your login: ");
+        Login = Console.ReadLine() ?? string.Empty;
+        
+        Console.Write("Enter your password: ");
+        Password = Console.ReadLine() ?? string.Empty;
+    }
+}
